b748b3a906833bcb33768bb1c04371ad145b7d55
Yang Gu <yang.gu@intel.com>
2012-07-03 17:50:36 +0800

    Add python script

---
+++ python/find-file.py
@@ -0,0 +1,15 @@
+# /usr/bin/python
+
+import os
+
+files = os.popen('find /workspace/project/BWS/img -type f -printf "%f\n"').readlines();
+
+for file in files:
+    name = file.rstrip('\n');
+    ret = os.system('grep -r ' + name + ' /workspace/project/BWS/* >/dev/null');
+    if ret != 0:
+        print name + " has no reference";
+        os.system("git rm /workspace/project/BWS/img/" + name);
+
+
+
