036ac7ce01aa14e32d37036f20b6a40479e03f70
Yang Gu <yang.gu@intel.com>
2012-12-06 13:44:13 +0800

    python: Add example of win32 functions

---
+++ python/win32.py
@@ -0,0 +1,12 @@
+﻿import win32gui
+import win32con
+
+pattern = 'Untitled - Notepad'
+
+def winEnumHandler(hwnd, ctx):
+    title = win32gui.GetWindowText(hwnd)
+    if title == pattern:
+        print hex(hwnd)
+        win32gui.SendMessage(hwnd, win32con.WM_CLOSE, 0, 0)
+
+win32gui.EnumWindows(winEnumHandler, None)
