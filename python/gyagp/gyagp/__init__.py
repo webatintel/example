58727ca8974805855635edab068a5fa3b9160e44
Yang Gu <yang.gu@intel.com>
2012-12-20 00:08:44 +0800

    python/gyagp: Add unit test

--- python/gyagp/gyagp/__init__.py
+++ python/gyagp/gyagp/__init__.py
@@ -3,11 +3,7 @@
 __version__ = "0.1.0"
 
 def check():
-    try:
-        import win32api
-        print('Module pywin32 has been installed')
-    except ImportError:
-        print('Module pywin32 has not been installed')
+    print('I am gyagp')
         
 __all__ = [
     # exceptions
--- python/gyagp/test/test_gyagp.py
+++ python/gyagp/test/test_gyagp.py
@@ -1,15 +1,26 @@
-def main():
-    try:
-        import gyagp
-        gyagp.check()
-    except ImportError:
-        print('Module sparrow has not been installed')
+import unittest
+
+class Test(unittest.TestCase):
+    def test_win32api(self):
+        try:
+            import win32api
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False) 
         
-    try:
-        from gyagp import module
-        module.check()
-    except ImportError:
-        print('Module sparrow has not been installed')        
-        
+    def test_gyagp(self):
+        try:
+            import gyagp
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False)   
+
+    def test_gyagp_module(self):
+        try:
+            from gyagp import module
+            self.assertTrue(True)
+        except ImportError:
+            self.assertTrue(False)            
+
 if __name__ == "__main__":
-    main()
+    unittest.main()
