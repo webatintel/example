f95a0dd94327dc28fdb38f05973f83f2ab654926
Yang Gu <yang.gu@intel.com>
2013-06-23 18:25:36 +0800

    python: Mouse simulation

--- python/util.py
+++ python/util.py
@@ -22,5 +22,11 @@ os.path.split(os.path.realpath(__file__))[0] # D:\gytemp\test
 ### Press enter, then exit ###
 raw_input("Press <enter>")
 
+### Mouse Simulation ###
+import win32api, win32con
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 
